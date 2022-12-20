460daafce1e597f9da6a28226326af89eeaaafb5
TuanPNT13 <phamnguyentrongtuan13@gmail.com>
2022-12-20 14:11:35 +0700

    Lan thu hai

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -6,6 +6,11 @@ public class MathUtilLive {
     public static void main(String[] args) {
         long result = MathUtil.getFactorial(5);
         System.out.println("5! = " + result);
+        
+        //Thêm code sau lần đầu tiên làm chuyện ấy - 2.10PM - 20/12/2022
+        System.out.println("1! = " + MathUtil.getFactorial(1));
+        System.out.println("3! = " + MathUtil.getFactorial(3));
+        
     }
     
 }
